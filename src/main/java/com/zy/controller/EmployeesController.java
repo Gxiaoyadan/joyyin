ac7bc371f6c8f719162927d21f88dea2b5e93f54
lgzhang <zl_gang@126.com>
2022-09-01 16:19:52 +0800

    新增jackson，对象转换json转换器（springmvc默认没有）

--- src/main/java/com/zy/controller/EmployeesController.java
+++ src/main/java/com/zy/controller/EmployeesController.java
@@ -1,6 +1,5 @@
 package com.zy.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.zy.pojo.Employees;
 import com.zy.service.EmployeesService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
+
 
 @Controller
 @RequestMapping("/employees")
@@ -20,12 +19,11 @@ public class EmployeesController {
 
     @RequestMapping(value = "/getEmployees")
     @ResponseBody
-    public String getAllEmployees() throws Exception {
+    public List<Employees> getAllEmployees() throws Exception {
         System.out.println("GET ONE");
-        System.out.println(employeesService.findAllEmployees());
-        String str = JSON.toJSONString(employeesService.findAllEmployees());
-        System.out.println(str);
-        return new String(str.getBytes(),"UTF-8");
+        List<Employees> empList = employeesService.findAllEmployees();
+        System.out.println(empList);
+        return employeesService.findAllEmployees();
     }
 
     @RequestMapping(value = "/getNameById")
